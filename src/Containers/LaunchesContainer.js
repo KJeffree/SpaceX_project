9b412516678178ae20480fb1454f7f603af69383
Katie Jeffree <k.jeffree@gmail.com>
2020-12-09 18:09:26 +0000

    set up state in LaunchesContainer, fetch launch data and rocket data successful

--- src/Containers/LaunchesContainer.js
+++ src/Containers/LaunchesContainer.js
@@ -2,6 +2,27 @@ import React, {useEffect, useState} from 'react'
 
 const LaunchesContainer = (props) => {
 
+    const [launches, setLaunches] = useState([])
+    const [rockets, setRockets] = useState([])
+    const [selectedYear, setSelectedYear] = useState(null)
+    const [sortAscending, setSortAscending] = useState(true)
+
+    useEffect(() => {
+        fetchLaunchData()
+        fetchRocketData()
+    }, [])
+
+    const fetchLaunchData = () => {
+        fetch("https://api.spacexdata.com/v4/launches")
+            .then(response => response.json())
+            .then(launches => setLaunches(launches))
+    }
+
+    const fetchRocketData = () => {
+        fetch("https://api.spacexdata.com/v4/rockets")
+            .then(response => response.json())
+            .then(rockets => setRockets(rockets))
+    }
     
     return(
         <p>I am a container</p>
