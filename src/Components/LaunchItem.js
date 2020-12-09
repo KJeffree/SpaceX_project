5ab81612af087e6d54fbca118b5182b9ddae38df
Katie Jeffree <k.jeffree@gmail.com>
2020-12-09 18:44:50 +0000

    use react moment to format date to display just day, short month and year

--- src/Components/LaunchItem.js
+++ src/Components/LaunchItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Moment from 'react-moment'
 
 const LaunchItem = (props) => {
     return (
         <React.Fragment>
             <p>Launch name: {props.launch.name}</p>
             <p>Rocket name: {props.rocket.name}</p>
-            <p>Launch date: {props.launch.date_utc}</p>
+            <p>Launch date: <Moment format="DD MMM YYYY">{props.launch.date_utc}</Moment></p>
             <p>Flight number: #{props.launch.flight_number}</p>
         </React.Fragment>
         
