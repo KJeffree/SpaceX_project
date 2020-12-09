b6a28be170d8628bb40803a4a6f5c23b94336da1
Katie Jeffree <k.jeffree@gmail.com>
2020-12-09 18:08:35 +0000

    initial setup

---
+++ src/Containers/LaunchesContainer.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react'
+
+const LaunchesContainer = (props) => {
+
+    
+    return(
+        <p>I am a container</p>
+    )
+}
+
+export default LaunchesContainer
