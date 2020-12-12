d3c6368ea52d2d364a35ec43f4149ee96a4c67a0
Katie Jeffree <k.jeffree@gmail.com>
2020-12-12 16:46:08 +0000

    filer by year functionality complete, including ability to clear filter

--- src/Containers/LaunchesContainer.js
+++ src/Containers/LaunchesContainer.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from 'react'
 import logo from '../public/assets/spacex-logo.png'
 import launchImage from '../public/assets/img/launch-home.png'
 import refreshIcon from '../public/assets/icon/refresh.png'
-import selectIcon from '../public/assets/icon/select.png'
 import sortIcon from '../public/assets/icon/sort.png'
 import LaunchesList from '../Components/LaunchesList'
 
@@ -10,7 +9,7 @@ const LaunchesContainer = (props) => {
 
     const [launches, setLaunches] = useState([])
     const [rockets, setRockets] = useState([])
-    // const [selectedYear, setSelectedYear] = useState(null)
+    const [selectedYear, setSelectedYear] = useState("All Years")
     const [sortAscending, setSortAscending] = useState(true)
 
     useEffect(() => {
@@ -32,11 +31,37 @@ const LaunchesContainer = (props) => {
 
     const onSortButtonClick = () => {
         setSortAscending(!sortAscending)
-        if (sortAscending){
-            setLaunches(launches.sort((a, b) => new Date(a.date_utc) - new Date(b.date_utc)))
-        } else {
-            setLaunches(launches.sort((a, b) => new Date(b.date_utc) - new Date(a.date_utc)))
+    }
+
+    const getLaunchesToDisplay = () => {
+        if (selectedYear != "All Years"){
+            return launches.filter(launch => launch.date_utc.substring(0, 4) === selectedYear)
         }
+        if (sortAscending){
+            return launches.sort((a, b) => new Date(a.date_utc) - new Date(b.date_utc))
+        } else {
+            return launches.sort((a, b) => new Date(b.date_utc) - new Date(a.date_utc))
+        }
+    }
+
+    const getUniqueYears = () => {
+        let uniqueYears = []
+        let launchYears = launches.map(launch => launch.date_utc.substring(0, 4))
+        launchYears.forEach(year => uniqueYears.includes(year) ? null : uniqueYears.push(year))
+        return uniqueYears
+    }
+
+    const optionNodes = () => {
+        const uniqueYears = getUniqueYears()
+        return uniqueYears.map(year => <option value={year} key={year} >{year}</option>)
+    }
+
+    const onYearSelected = (event) => {
+        setSelectedYear(event.target.value)
+    }
+
+    const resetSelectedYear = () => {
+        setSelectedYear("All Years")
     }
 
     return(
@@ -46,15 +71,19 @@ const LaunchesContainer = (props) => {
                 <img className="logo" alt="SpaceX logo" src={logo}></img>
                 <span className="page-name">LAUNCHES</span>
             </div>
-            <button className="reload-data-button"><b>Reload Data</b><img alt="reload icon" className="icon" src={refreshIcon}></img></button>
+            <button onClick={getUniqueYears} className="button" id="reload-data-button">Reload Data<img alt="reload icon" className="icon" src={refreshIcon}></img></button>
         </div>
         <div className="filtering">
-            <button><b>Filter by Year</b><img alt="select icon" className="icon" src={selectIcon}></img></button>
-            <button onClick={onSortButtonClick}><b>Sort {sortAscending ? "Descending" : "Ascending"}</b><img alt="sort icon" className="icon" src={sortIcon}></img></button>
+            <p className="filter-clear" hidden={selectedYear === "All Years"} onClick={resetSelectedYear}>Clear Filter x</p>
+            <select className="button" onChange={onYearSelected} value={selectedYear}>
+                <option value="All Years" disabled>Fliter by Year</option>
+                {optionNodes()}
+            </select>
+            <button className="button" onClick={onSortButtonClick}>Sort {sortAscending ? "Descending" : "Ascending"}<img alt="sort icon" className="icon" src={sortIcon}></img></button>
         </div>
         <div className="main-section">
             <img src={launchImage} alt="rocket launch" className="launch-image"></img>
-            <LaunchesList launches={launches} rockets={rockets}></LaunchesList>
+            <LaunchesList launches={getLaunchesToDisplay()} rockets={rockets}></LaunchesList>
         </div>
             
         </>
